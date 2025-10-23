463218a55d7e57a789a12ae5a5e6a1493bb4922a
MD Fahad Molla <ahfahad47sss@gmail.com>
2025-10-23 16:56:29 +0600

    navbar bug fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link, NavLink } from "react-router";
 import logoImg from "../assets/logo.png";
 import { AuthContext } from "../provider/AuthContext";
 import { toast } from "react-toastify";
-import avaterImg from '../assets/avater.png'
+import avaterImg from "../assets/avater.png";
 const Navbar = () => {
   const { user, setUser, loading, setLoading, signoutUserFunc } =
     use(AuthContext);
@@ -19,7 +19,7 @@ const Navbar = () => {
       });
   };
 
-    console.log(loading);
+  console.log(loading);
 
   const navLinks = (
     <>
@@ -32,11 +32,11 @@ const Navbar = () => {
       <li>
         <NavLink to="/about">About</NavLink>
       </li>
-     {
-      !user &&  <li>
-        <NavLink to="/register">Register</NavLink>
-      </li>
-     }
+      {!user && (
+        <li>
+          <NavLink to="/register">Register</NavLink>
+        </li>
+      )}
     </>
   );
 
@@ -44,7 +44,11 @@ const Navbar = () => {
     <div className="navbar  shadow-sm bg-[#262626] text-white">
       <div className="navbar-start">
         <div className="dropdown">
-          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+          <div
+            tabIndex={0}
+            role="button"
+            className=" px-4 py-3 mr-1 btn-ghost  lg:hidden cursor-pointer "
+          >
             <svg
               xmlns="http://www.w3.org/2000/svg"
               className="h-5 w-5"
@@ -63,24 +67,29 @@ const Navbar = () => {
           </div>
           <ul
             tabIndex="-1"
-            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+            className="menu menu-sm dropdown-content bg-[#262626] rounded-box z-1 mt-3 w-52 p-2 shadow "
           >
             {navLinks}
           </ul>
         </div>
-        <NavLink to="/" className="btn btn-ghost text-xl">
+        <NavLink
+          to="/"
+          className="flex justify-center items-center  md:ml-2 font-bold btn-ghost pl-0  text-xl"
+        >
           {" "}
-          <img className="mr-2" src={logoImg} alt="" /> Game{" "}
-          <span className="text-[#EE0001]">Hub</span>{" "}
+          <img
+            className="mr-1 lg:mr-2 md:mr-2  "
+            src={logoImg}
+            alt=""
+          /> Game <span className="text-[#EE0001]">Hub</span>{" "}
         </NavLink>
       </div>
 
       <div className="navbar-end">
         <ul className="menu menu-horizontal px-1 hidden lg:flex">{navLinks}</ul>
-      {user ? (
+        {user ? (
           <div className="flex justify-between items-center text-center gap-4">
-            <Link to="/profile"
-            >
+            <Link to="/profile">
               <img
                 src={user?.photoURL || avaterImg}
                 className="h-[40px] w-[40px] rounded-full mx-auto"
@@ -88,12 +97,12 @@ const Navbar = () => {
               />
             </Link>
 
-            
-              
-              <button onClick={handleSignout} className="bg-red-700 text-white px-4 py-2 rounded-md font-semibold cursor-pointer">
-                Logout
-              </button>
-         
+            <button
+              onClick={handleSignout}
+              className="bg-red-700 text-white px-4 py-2 rounded-md font-semibold cursor-pointer"
+            >
+              Logout
+            </button>
           </div>
         ) : (
           <button className="bg-red-700 text-white px-4 py-2 rounded-md font-semibold cursor-pointer">
