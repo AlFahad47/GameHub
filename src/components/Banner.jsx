e268346e25535d369a52e37e937445bbeb63ef07
MD Fahad Molla <ahfahad47sss@gmail.com>
2025-10-23 15:35:54 +0600

    banner bug fixed

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
+import React from "react";
 import Slider from "react-slick";
 
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-import bannerImg1 from '../assets/banner1.png';
-import bannerImg2 from '../assets/banner2.jpg';
-import bannerImg3 from '../assets/banner3.jpg';
+import bannerImg1 from "../assets/banner4.webp";
+import bannerImg2 from "../assets/banner2.jpg";
+import bannerImg3 from "../assets/banner3.jpg";
 const Banner = () => {
-     const slides = [
-    { id: 1, img: bannerImg1 },
-    { id: 2, img: bannerImg2 },
+  const slides = [
+    { id: 1, img: bannerImg2 },
+    { id: 2, img: bannerImg1 },
     { id: 3, img: bannerImg3 },
   ];
 
@@ -24,18 +24,19 @@ const Banner = () => {
   };
 
   return (
-    <div className=' lg:w-full w-11/12 mx-auto max-w-5xl mt-8 rounded-2xl overflow-hidden shadow-lg'>
-      <Slider  {...settings}>
-        {slides.map(slide=>(
-            <div key={slide.id} className='relative'>
-            <img  className="w-full  min-h-60  object-center object-cover" src={slide.img} />
-            
-            </div>
+    <div className=" lg:w-full w-11/12 mx-auto lg:h-[290px]  md:h-60 h-[115px] max-w-5xl mt-8 rounded-2xl overflow-hidden shadow-lg">
+      <Slider {...settings}>
+        {slides.map((slide) => (
+          <div key={slide.id} className="relative">
+            <img
+              className="w-full    object-center  object-cover"
+              src={slide.img}
+            />
+          </div>
         ))}
       </Slider>
-     
     </div>
-  )
-}
+  );
+};
 
-export default Banner
+export default Banner;
