716999f68a3cf310ce0a4ceabc435032cd8e368c
MD Fahad Molla <ahfahad47sss@gmail.com>
2025-10-23 13:06:11 +0600

    card hover effect added using motion

--- src/components/GameCard.jsx
+++ src/components/GameCard.jsx
@@ -1,26 +1,31 @@
 import React from "react";
 import { Link, Navigate } from "react-router";
 import ratingImg from "../assets/star.png";
+import { motion } from "framer-motion";
 
 const GameCard = ({ game }) => {
-  const handleClick = () => {};
   return (
-    <Link
-      to={`/game/${game.id}`}
-      className="w-[370px] max-h-[550px] bg-[#AD2B2B] rounded-md flex flex-col justify-between cursor-pointer "
-    >
-      <img
-        className="rounded-t-md h-[420px] object-cover"
-        src={game.coverPhoto}
-        alt=""
-      />
-      <div className="pl-2 lg:pl-4 pb-2 lg:pb-4">
-        <h2 className="text-white font-medium mt-2.5">{game.title}</h2>
-        <div className="flex py-1.5  gap-2  items-center">
-          <img src={ratingImg} className="w-4 h-4" alt="" />
-          <p className="">{game.ratings}</p>
+    <Link to={`/game/${game.id}`}>
+      <motion.div
+        whileHover={{ scale: 1.1 }}
+        whileTap={{ scale: 1 }}
+        className="w-[370px] max-h-[550px] bg-[#AD2B2B] rounded-md flex flex-col justify-between cursor-pointer "
+      >
+        <div>
+          <img
+            className="rounded-t-md h-[420px] object-cover"
+            src={game.coverPhoto}
+            alt=""
+          />
+          <div className="pl-2 lg:pl-4 pb-2 lg:pb-4">
+            <h2 className="text-white font-medium mt-2.5">{game.title}</h2>
+            <div className="flex py-1.5  gap-2  items-center">
+              <img src={ratingImg} className="w-4 h-4" alt="" />
+              <p className="">{game.ratings}</p>
+            </div>
+          </div>
         </div>
-      </div>
+      </motion.div>
     </Link>
   );
 };
