866124c6c57facc7e286b132458940b1f7850cc7
MD Fahad Molla <ahfahad47sss@gmail.com>
2025-10-25 13:51:43 +0600

    error page added

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,10 +1,26 @@
 import React from "react";
 import { useTitle } from "../hooks/useTitle";
+import errorImg from "../assets/error.png";
+import { Link } from "react-router";
 
 const PageNotFound = () => {
   useTitle("Page not Found | GameHub");
 
-  return <div>404 page not found</div>;
+  return (
+    <div className="flex flex-col justify-center items-center  h-screen bg-red-700">
+      <img src={errorImg} className="text-white" alt="" />
+      <div className="text-black text-center mt-5">
+        <h2 className="font-bold text-4xl">GAME OVER</h2>
+        <p className="mt-2.5">OOPS PAGE NOT FOUND</p>
+      </div>
+      <Link
+        to="/"
+        className="bg-black px-5 py-2.5 mt-2.5 text-red-600  font-semibold"
+      >
+        Return Home
+      </Link>
+    </div>
+  );
 };
 
 export default PageNotFound;
